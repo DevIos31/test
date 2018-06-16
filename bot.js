935beb6d62c204db663bcdfedfccb729a21d1290
NOVA52 <34939959+NOVA52@users.noreply.github.com>
2018-06-16 10:28:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,8 +35,6 @@ client.on('message', message => {
     	message.reply('owner/devloper of the game called DeltaSquad ');
   	}
 });
-if (fromID === me && text.includes("say") ) {
-    bot.sendMessage({ to: channel, message: txt.replace('!say ','') });
-}
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
