584e75c52cf3ad187e1458802da8379888f01a2a
NOVA52 <34939959+NOVA52@users.noreply.github.com>
2018-06-16 12:22:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,41 +10,5 @@ client.on('message', message => {
     	message.reply('hey ');
   	}
 });
-client.on('message', message => {
-    if (message.content === 'anyone here') {
-    	message.reply('yes i am here how can i help you');
-  	}
-});
-client.on('message', message => {
-    if (message.content === 'whats up') {
-    	message.reply('fine and you');
-  	}
-});
-client.on('message', message => {
-    if (message.content === '?who is Antares Qlapard') {
-    	message.reply('A cool moderator of a game called DeltaSquad');
-  	}
-});
-client.on('message', message => {
-    if (message.content === '?who is rgt') {
-    	message.reply('A cool Admin of a game called DeltaSquad ');
-  	}
-});
-client.on('message', message => {
-    if (message.content === '?who is devios') {
-    	message.reply('owner/devloper of the game called DeltaSquad ');
-  	}
-});
-client.on('message', message => {
-    if (message.content === 'help') {
-    	message.reply('go ask mod i am lazy ');
-    }
-});
-client.on('message', message => {
-    if (message.content === '?who is  KrishPhadte') {
-    	message.reply('A cool moderator of a game called DeltaSquad');
-    }
-});
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
