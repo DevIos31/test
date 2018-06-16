3bb17d213f725d5c25bd531c5494bade93530a59
NOVA52 <34939959+NOVA52@users.noreply.github.com>
2018-06-16 10:34:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,17 +35,6 @@ client.on('message', message => {
     	message.reply('owner/devloper of the game called DeltaSquad ');
   	}
 });
-	if (command == "help") {
-		const embed = new Discord.RichEmbed()
-		.setColor(0x954D23)
-		.setTitle("Command List:")
-		.addField("!help", "Will give the current command list")
-		.addField("!ping", "WIll show the ping time for the bot")
-		.addField("!say [text]", "Will make the bot say something")
-		.addField("!announcement [text]", "Will make the bot say an announcement and tag everyone")
-		.addField("!cat", "Will send a random cat image");
-		message.channel.send({embed})
-	};
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
