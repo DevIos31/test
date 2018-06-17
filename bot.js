3875a2c75006ba305c177ad51d56e07a26abda77
NOVA52 <34939959+NOVA52@users.noreply.github.com>
2018-06-17 10:10:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,40 +31,40 @@ client.on('message', message => {
   	}
 });
 client.on('message', message => {
--    if (message.content === 'anyone here') {
--    	message.reply('yes i am here how can i help you');
--  	}
--});
--client.on('message', message => {
--    if (message.content === 'whats up') {
--    	message.reply('fine and you');
--  	}
--});
--client.on('message', message => {
--    if (message.content === '?who is Antares Qlapard') {
--    	message.reply('A cool moderator of a game called DeltaSquad');
--  	}
--});
--client.on('message', message => {
--    if (message.content === '?who is rgt') {
--    	message.reply('A cool Admin of a game called DeltaSquad ');
--  	}
--});
--client.on('message', message => {
--    if (message.content === '?who is devios') {
--    	message.reply('owner/devloper of the game called DeltaSquad ');
--  	}
--});
--client.on('message', message => {
--    if (message.content === 'help') {
--    	message.reply('go ask mod i am lazy ');
--    }
--});
--client.on('message', message => {
--    if (message.content === '?who is  KrishPhadte') {
--    	message.reply('A cool moderator of a game called DeltaSquad');
--    }
--});
+    if (message.content === 'anyone here') {
+    	message.reply('yes i am here how can i help you');
+  	}
+});
+client.on('message', message => {
+   if (message.content === 'whats up') {
+   	message.reply('fine and you');
+ 	}
+});
+client.on('message', message => {
+    if (message.content === '?who is Antares Qlapard') {
+    message.reply('A cool moderator of a game called DeltaSquad');
+  	}
+});
+client.on('message', message => {
+    if (message.content === '?who is rgt') {
+   	message.reply('A cool Admin of a game called DeltaSquad ');
+  	}
+});
+client.on('message', message => {
+    if (message.content === '?who is devios') {
+    	message.reply('owner/devloper of the game called DeltaSquad ');
+  	}
+});
+client.on('message', message => {
+   if (message.content === 'help') {
+    	message.reply('go ask mod i am lazy ');
+    }
+});
+client.on('message', message => {
+   if (message.content === '?who is  KrishPhadte') {
+  	message.reply('A cool moderator of a game called DeltaSquad');
+    }
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
