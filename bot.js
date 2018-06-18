67e6d902141c143ca01677849e6e62647c8f3d43
NOVA52 <34939959+NOVA52@users.noreply.github.com>
2018-06-18 16:43:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  client.user.setGame('DeltaSquad');
+client.User.setGame("test")
     console.log('I am ready!');
 });
 
