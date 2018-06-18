ff98304fccb72a168d0eb4de78653072760c0e24
NOVA52 <34939959+NOVA52@users.noreply.github.com>
2018-06-18 18:07:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setGame('Test')
+    client.user.setGame('Test');
     console.log('I am ready!');
 });
 
