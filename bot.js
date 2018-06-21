4767c8dae159358a928eba6fe33fa5d6efd4b035
NOVA52 <34939959+NOVA52@users.noreply.github.com>
2018-06-21 19:25:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,9 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setGame('Test');
+    client.user.setPresence({ game: { name: 'with discord.js' }, status: 'idle' })
+  .then(console.log)
+  .catch(console.error);
     console.log('I am ready!');
 });
 
